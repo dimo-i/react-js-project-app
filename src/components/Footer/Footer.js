913c6585965c48a362023113e7a188605d9ec657
dimo-i <81874988+dimo-i@users.noreply.github.com>
2022-08-07 18:54:40 +0300

    initial commit
    Initial commit of client

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return (
+        <footer className="site-footer fixed-bottom">
+            <div>SoftUni - React JS Final Project - August 2022</div>
+            <div><a href="#">Dimo Ivanov</a></div>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <span>React JS Project App August 2022</span>
+            </div>
+
+        </header>
+    )
+}
